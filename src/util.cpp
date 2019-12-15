e9e668bf7ba5038e1f17c0ee9d255dd146230066
George Lewis <georgejeffreylewis@gmail.com>
2019-12-15 12:23:08 -0500

    Fix vector normalizing algortithm

--- src/util.cpp
+++ src/util.cpp
@@ -4,7 +4,11 @@ void Util::normalize(sf::Vector2f& vec) {
 
     float magnitude = sqrt(vec.x*vec.x + vec.y*vec.y);
 
-    vec.x = vec.x / magnitude;
-    vec.y = vec.y / magnitude;
+    if (magnitude > 0) {
+
+        vec.x = vec.x / magnitude;
+        vec.y = vec.y / magnitude;
+
+    }
 
 }
